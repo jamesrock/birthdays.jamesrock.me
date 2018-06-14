b4b6accfef97e14d069d0cd2e0de0daeeacf1739
James Rock <hello@jamesrock.me>
2018-06-14 07:49:39 +0100

    updates

--- js/birthdays.js
+++ js/birthdays.js
@@ -1,23 +1,66 @@
 (function() {
 
 	var
-	birthdays = [
-		['08/01/1989', 'Me'],
-		['02/12/1980', 'Alice'],
-		['26/03/2013', 'Harris'],
-		['18/04/2015', 'Arran'],
-		['02/12/1980', 'Charlotte'],
-		['02/12/1980', 'Thomas'],
-		['04/12/1967', 'Mum'],
-		['23/06/1946', 'Gran'],
-		['04/09/1980', 'Granddad'],
-		// ['02/12/1980', 'Alan'],
-		// ['02/12/1980', 'Neil'],
-		['02/12/1980', 'Ash'],
-		// ['02/12/1980', 'Bliss'],
-		['20/10/2014', 'Dollie-Mae']
-	];
+	root = document.getElementById('root'),
+	birthdays = [],
+	Birthday = function(date, name) {
 
-	console.log('birthdays', birthdays);
+		this.date = date;
+		this.name = name;
+
+	},
+	createBirthday = function(date, name) {
+
+		birthdays.push(new Birthday(date, name));
+
+	},
+	render = function() {
+
+		var
+		out = '';
+
+		birthdays.forEach(function(birthday) {
+
+			out += birthday.toHTML();
+
+		});
+
+		root.innerHTML = out;
+
+		return out;
+
+	};
+
+	Birthday.prototype.getSortableDate = function() {
+
+		return this.date.split('/').reverse().join('');
+
+	};
+	Birthday.prototype.toHTML = function() {
+
+		return `<div>${this.name}</div>`;
+
+	};
+
+	createBirthday('08/01/1989', 'Me');
+	createBirthday('02/12/1980', 'Alice');
+	createBirthday('26/03/2013', 'Harris');
+	createBirthday('18/04/2015', 'Arran');
+	createBirthday('23/11/1993', 'Charlotte');
+	createBirthday('04/06/1999', 'Thomas');
+	createBirthday('04/12/1967', 'Mum');
+	createBirthday('23/06/1946', 'Gran');
+	createBirthday('04/09/1945', 'Granddad');
+	createBirthday('20/10/2014', 'Dollie-Mae');
+	// createBirthday('02/12/1980', 'Alan');
+	// createBirthday('02/12/1980', 'Neil');
+	// createBirthday('02/12/1980', 'Ash');
+	// createBirthday('02/12/1980', 'Bliss');
+
+	birthdays.sort(ROCK.SORT.NUMBER_ASCENDING(function() {
+		return this.getSortableDate();
+	}));
+
+	render();
 
 })();
